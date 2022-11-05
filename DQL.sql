656082aebad5b8b43921553ca987b204965c7155
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2022-11-04 21:05:46 -0300

    First Commit

---
+++ DQL.sql
@@ -0,0 +1,88 @@
+USE GalloFlixDb;
+-- SQL
+-- DQL - Data Query Language
+-- Exibir todos os campos dos filmes cadastrados
+
+
+-- Exibir todos os campos dos filmes cadastrados, ordenados por título
+
+
+-- Exibir todos os campos dos filmes lançados em 2006
+
+
+-- Exibir todos os filmes com classificação etária livre
+
+
+-- Exibir o título, duração e ano de estreia dos filmes com mais de 150 minutos, ordenados do mais longo para o mais curto
+
+
+-- Exibir o título, duração e ano de estreia dos filmes lançados entre 1990 e 2010, renomeando os campos para português
+
+
+-- Exibir o título, duração e ano de estreia dos filmes lançados entre 1990 e 2010 com duração maior que 150 minutos, renomeando os campos para português
+
+
+-- Exibir todos os filmes que estreiaram nos anos de 1996, 1999, 2005 e 2010 
+
+
+-- Exibir todos os filmes que contenham a palavra 'Guerra' em seu título
+
+
+-- Exibir apenas os anos que possuem filmes cadastrados (não exibir os filmes, apenas os anos)
+
+
+-- ////////////////////////////////////////////////////////////////////////////////////////////////////////
+-- FUNÇÕES AGREGADAS
+-- /////////////////////////////////////////////////////////////////
+-- CONTAGEM - COUNT
+-- Exibir a quantidade de filmes que estreiam entre 1990 e 2010
+ 
+ 
+-- Exibir a quantidade de filmes com duração maior que 150 minutos
+
+ 
+-- Curiosidade 1 - Exibir os anos e a quantidade de filmes lançados naquele ano
+ 
+-- ///////////////////////////////////////////////////////////////// 
+-- MÁXIMO - MAX
+-- Maior ocorrência (valor) de um campo 
+-- Exibir a maior duração existente entre todos os filmes cadastrados
+ 
+
+-- Exibir o ano de lançamento mais recente entre os filmes cadastrados
+
+
+-- /////////////////////////////////////////////////////////////////
+-- MÍNIMO - MIN
+-- Menor ocorrência (valor) de um campo 
+-- Exibir a menor  duração existente entre os filmes cadastrados
+
+
+-- Exibir o ano de lançamento mais antigo entre os filmes cadastrados
+
+
+-- /////////////////////////////////////////////////////////////////
+-- SOMATÓRIO - SUM
+-- Exibir o total de tempo necessário para assistir os filmes que estreiaram entre 1990 e 2010
+
+
+-- /////////////////////////////////////////////////////////////////
+-- MÉDIA - AVG
+-- A média de duração dos filmes que estreiaram entre 1990 e 2010
+ 
+ 
+-- ////////////////////////////////////////////////////////////////////////////////////////////////////////
+-- SUBCONSULTAS
+-- Exibir o filme com a maior (maximo) duração
+-- Sem usar subconsulta - atualização do comando manual
+
+-- Com subconsulta - atualização dinâmica
+
+
+
+-- Exibir os filmes do Gênero Terror
+-- Sem usar subconsulta - atualização do comando manual
+
+-- Com subconsulta - atualização dinâmica
+
+ 
